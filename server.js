2f7dab361c979dfcac8a7cb8b2bb4357d0532d8c
Kyrylo Chebotarov <kparusik@gmail.com>
2020-06-15 00:40:52 +0300

    add engine

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const publicPath = path.join(__dirname, './build');
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.static(publicPath));
+app.use(cors())
+app.get('*', (req, res) => res.sendFile(path.join(publicPath, 'index.html')));
+app.listen(port, () => console.log('Server is up!'));
